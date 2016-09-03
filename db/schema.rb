44911cf1120b1ca02e31368911446d17d278fff2
Lukas <Lukasbarry@yahoo.com>
2016-09-03 15:21:47 -0400

    user added to buyers and formatting updated

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160814194310) do
+ActiveRecord::Schema.define(version: 20160903192018) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -40,7 +40,10 @@ ActiveRecord::Schema.define(version: 20160814194310) do
     t.string   "alta_endorsement"
     t.datetime "created_at",                   null: false
     t.datetime "updated_at",                   null: false
+    t.integer  "user_id"
   end
+
+  add_index "buyers", ["user_id"], name: "index_buyers_on_user_id", using: :btree
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
@@ -68,4 +71,5 @@ ActiveRecord::Schema.define(version: 20160814194310) do
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
+  add_foreign_key "buyers", "users"
 end
