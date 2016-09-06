79bdcc9f6182673da57898fe3e09e174b8fbbdc9
Lukas <Lukasbarry@yahoo.com>
2016-09-06 09:17:49 -0400

    fix buyer association

--- app/models/buyer.rb
+++ app/models/buyer.rb
@@ -2,5 +2,5 @@ class Buyer < ActiveRecord::Base
   validates :current_owner, :borrower, :property_address_street, presence: true
   validates :property_address_number, numericality: {greater_than_or_equal_to: 0}
   belongs_to :user
-  has-many :title_reports
+  has_one :title_report
 end
